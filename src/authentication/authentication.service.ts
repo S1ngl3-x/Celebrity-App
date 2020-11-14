b95208ded18c30b3aebf05dc513f07b40ad67d69
lipowada <lipowada@fel.cvut.cz>
2020-11-14 12:31:23 +0100

    :bug: Fix cookie cors bug

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -43,7 +43,7 @@ export class AuthenticationService {
   public getCookieWithJwtToken(userId: number) {
     const payload: TokenPayload = { userId };
     const token = this.jwtService.sign(payload);
-    return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get(
+    return `Authentication=${token}; HttpOnly; Path=/; SameSite=None; Max-Age=${this.configService.get(
       'JWT_EXPIRATION_TIME',
     )}`;
   }
